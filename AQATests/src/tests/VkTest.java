03cb29f0a862cced3a37a42ef5109892d1b9f5f8
Daniel Khomenko <homenko.daniel@gmail.com>
2017-05-15 15:16:59 +0300

    Update VkTest.java

--- AQATests/src/tests/VkTest.java
+++ AQATests/src/tests/VkTest.java
@@ -9,14 +9,14 @@ public class VkTest extends BrowserFactory{
     @Test
     public void sendBirthdayMsg() {
         VkMainPage vk = new VkMainPage();
-        vk.logIn("mrbravo@bigmir.net", "2342925bravo");
+        vk.logIn("mrbravo@bigmir.net", "234000vo");
         vk.sendBdayMsg();
     }
 
     @Test
     public void showMessages() throws InterruptedException {
         VkMainPage vk = new VkMainPage();
-        vk.logIn("dan.dudelife.bravo@gmail.com", "4ezahuin9");
+        vk.logIn("dan.dudelife.bravo@gmail.com", "498090uin9");
         vk.openMessages();
         Thread.sleep(3000);
     }
